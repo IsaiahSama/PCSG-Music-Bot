c23883c85938b117f14bf619573c81be71fda6b4
IsaiahSama <zeldaplayergl@gmail.com>
2020-10-06 15:49:40 -0400

    Fixed up exp, leveling and help

--- mod/PCSGMod.py
+++ mod/PCSGMod.py
@@ -4,17 +4,41 @@ import asyncio
 import os
 from dotenv import load_dotenv
 load_dotenv()
+from random import randint
 
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="^>", case_insensitive=True, intents=intents)
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="p.", case_insensitive=True, intents=intents)
 bot.help_command = None
 
 bot.load_extension("roles")
+bot.load_extension("levels")
+
+@bot.event
+async def on_ready():
+    print(f"Right...")
+    activity = discord.Activity(name='for p.help', type=discord.ActivityType.watching)
+    await bot.change_presence(activity=activity)
 
 @bot.command()
+@commands.is_owner()
 async def rc(ctx):
     bot.reload_extension("roles")
+    bot.reload_extension("levels")
     await ctx.send("Cogs Have been reloaded")
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(
+        title="Showing Help",
+        color=randint(0, 0xffffff)
+    )
+
+    embed.set_thumbnail(url=bot.user.avatar_url)
+    embed.add_field(name="p.profile", value="Shows your profile for this server", inline=False)
+    embed.add_field(name="p.top", value="Shows top 5 active users", inline=False)
+    embed.add_field(name="p.rank", value="Shows your rank for this server", inline=False)
+    
+    await ctx.send(embed=embed)
 
 
 yes = os.getenv("keymod")
---
+++ mod/levels.py
@@ -0,0 +1,143 @@
+import discord
+from discord.ext import commands, tasks
+import random
+from random import randint
+import asyncio
+import aiosqlite
+
+
+class Person:
+    def __init__(self, name, tag, level, exp, expthresh):
+        self.name = name
+        self.tag = tag
+        self.level = level
+        self.exp = exp
+        self.expthresh = expthresh
+
+    def incexp(self):
+        self.exp += 5
+
+    def levelup(self):
+        self.exp = 0
+        self.expthresh += 50
+        self.level += 1
+
+    def didrole(self):
+        if self.level % 20 == 0:
+            return True
+        return False
+
+
+class Leveling(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        bot.loop.create_task(self.async_init())
+
+    users = []
+    roles = []
+
+    async def async_init(self):
+        await self.bot.wait_until_ready()
+        async with aiosqlite.connect("PCSGDB.sqlite3") as db:
+            await db.execute("""CREATE TABLE IF NOT EXISTS Users (
+                Name TEXT NOT NULL,
+                ID INTEGER PRIMARY KEY UNIQUE NOT NULL,
+                Level INTEGER NOT NULL,
+                Exp INTEGER NOT NULL,
+                ExpThresh INTEGER NOT NULL
+                );""")
+
+        await self.setup()
+        await self.saving.start()
+
+    async def setup(self):
+        guild = self.bot.get_guild(693608235835326464)
+        self.roles = [role.name for role in guild.roles if role.name.endswith("Learner")]
+        print(self.roles)
+        async with aiosqlite.connect("PCSGDB.sqlite3") as db:
+            for member in guild.members:
+                if member.bot: continue
+                
+                await db.execute("INSERT OR IGNORE INTO Users (Name, ID, Level, Exp, ExpThresh) VALUES (?, ?, ?, ?, ?)",
+                (member.name, member.id, 0, 0, 50))
+
+            await db.commit()
+
+            async with db.execute("SELECT * FROM Users") as cursor:
+                async for row in cursor:
+                    x = Person(row[0], row[1], row[2], row[3], row[4])
+                    self.users.append(x)
+
+                print("All users have been added")
+
+    @commands.command()
+    async def profile(self, ctx):
+        person = await self.getperson(ctx)
+        if not person:
+            return
+
+        probed = discord.Embed(
+            title=f"Showing Profile for {ctx.author}",
+            color=randint(0, 0xffffff)
+        )
+
+        probed.set_thumbnail(url=ctx.author.avatar_url)
+        probed.add_field(name="Name:", value=person.name)
+        probed.add_field(name="Level:", value=person.level)
+        probed.add_field(name="Exp:", value=f"{person.exp}/{person.expthresh}")
+
+        await ctx.send(embed=probed)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member:discord.Member):
+        async with aiosqlite.connect("PCSGDB.sqlite3") as db:
+            await db.execute("INSERT OR IGNORE INTO Users (Name, ID, Level, Exp, ExpThresh) VALUES (?, ?, ?, ?, ?)",
+            (member.name, member.id, 0, 0, 50))
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        
+        if message.author.bot: return
+
+        person = await self.getperson(message)
+        if not person:
+            return
+
+        person.incexp()
+        if person.exp >= person.expthresh:
+            person.levelup()
+
+            embed = discord.Embed(
+                title="LEVEL UP",
+                description=f"{message.author.mention} has reached level {person.level}. Keep Studying hard",
+                color=randint(0, 0xffffff)
+            )
+            if person.didrole():
+                role_to_give = self.roles[person.level / 20]
+                await message.author.add_role(role_to_give)
+
+                embed.add_field(name="New role", value=f"Congrats {message.author.name}. You now have the role of {role_to_give.name}")
+
+            await message.channel.send(embed=embed)
+        
+        
+    @tasks.loop(minutes=5)
+    async def saving(self):
+        async with aiosqlite.connect("PCSGDB.sqlite3") as db:
+            for member in self.users:
+                await db.execute("INSERT OR IGNORE INTO Users (Name, ID, Level, Exp, ExpThresh) VALUES (?, ?, ?, ?, ?)",
+                (member.name, member.tag, member.level, member.exp, member.expthresh))
+
+        print("Saved")
+
+    async def getperson(self, m):
+        toreturn = [x for x in self.users if m.author.id == x.tag]
+        if not toreturn:
+            await m.channel.send("Something went wrong getting your user")
+            return None
+        return toreturn[0]
+
+
+def setup(bot):
+    bot.add_cog(Leveling(bot))
