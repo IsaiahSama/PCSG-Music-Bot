49dae27da3ef33a61a3c681e25664acbd08966d3
IsaiahSama <zeldaplayergl@gmail.com>
2020-10-06 10:51:39 -0400

    Fixed bugs and functionality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-# os.system("start lofi.py")
-# os.system("start nature.py")
-# os.system("start piano.py")
+os.system("start lofi.py")
+os.system("start nature.py")
+os.system("start piano.py")
 os.system("start timer1.py")
 os.system("start timer2.py")
--- timez.py
+++ timez.py
@@ -8,41 +8,65 @@ class Timer(commands.Cog):
         self.bot = bot
         bot.loop.create_task(self.async_init())
 
+    guild = None
     async def async_init(self):
         await self.bot.wait_until_ready()
-        guild = self.bot.get_guild(693608235835326464)
+        self.guild = self.bot.get_guild(693608235835326464)
+        
+        await self.connect(self.guild)
 
-        await self.connect(guild)
-
+    channel = None
     async def connect(self, guild):
+        # 45 minute bot
         if self.bot.user.id == 762840423965392906:
-            channel = guild.get_channel(762849851557675009)
+            self.channel = guild.get_channel(762849851557675009)
+        # 25 minute bot
         elif self.bot.user.id == 762840289417101352:
-            channel = guild.get_channel(762849722712064031)
+            self.channel = guild.get_channel(762849722712064031)
 
+        channel = self.channel
         await channel.connect()
+        print("We connected")
         await self.prepare(guild, channel)
 
     async def prepare(self, guild, channel):
         cname = channel.name.split(" ")
         cname[-1] = cname[0]
         await channel.edit(name=" ".join(cname))
-        await self.ticker(guild, channel, cname)       
+        await asyncio.sleep(300)
+        await self.ticker.start()  
+            
 
-    async def ticker(self, guild, channel, ogtime):
-        while True:
-            curname = channel.name.split(" ")
-            curtime = int(curname[-1])
-            while curtime > 0:
-                await asyncio.sleep(5)
-                curtime -= 1
+    @tasks.loop(minutes=5)
+    async def ticker(self):
+        channel = self.channel
+        curname = channel.name.split(" ")
+        ogtime = curname[0]
+        curtime = int(curname[-1])
+        curtime -= 5
 
-                curname[-1] = str(curtime)
-                await channel.edit(name=" ".join(curname))
-                    
+        if curtime <= 0:                
             await channel.edit(name=f"{ogtime} Min Study: On Break")
-            await asyncio.sleep(20)
+            await asyncio.sleep(300)
             await channel.edit(name=f"{ogtime} Min Study Time: {ogtime}")
+
+        else:
+            curname[-1] = str(curtime)
+            await channel.edit(name=" ".join(curname))
+
+
+    @tasks.loop(seconds=45)
+    async def recon(self):
+        if self.guild.voice_client == None:
+            await self.channel.connect()
+
+    @ticker.after_loop
+    async def rentick(self):
+        await self.ticker.start()
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        print(error)
 
                   
 
