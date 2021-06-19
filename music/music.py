06d776acda8449bec030c73904f5526f15adc194
Isaiah-Sama <zeldaplayergl@gmail.com>
2021-06-19 14:39:15 -0400

    Did a rewrite of the music bots

--- music/music.py
+++ music/music.py
@@ -9,66 +9,90 @@ class Music(commands.Cog):
 
     data_dict = {}
 
+    bot_dict = {
+        755685507907846144: {
+            "BOT_NAME": "Lofi Bot",
+            "CHANNEL_ID": 762082834235654164,
+            "TRACK": "lofi.mp3"
+        },
+        756347306038657085: {
+            "BOT_NAME": "Nature Bot",
+            "CHANNEL_ID": 762150460651339806,
+            "TRACK": "nature.mp3"
+        },
+        762167641334218762: {
+            "BOT_NAME": "Piano Bot",
+            "CHANNEL_ID": 762171605244968990,
+            "TRACK": "piano.mp3"
+        },
+        820792275743014915: {
+            "BOT_NAME": "Relax and vibe",
+            "CHANNEL_ID": 820793494419144845,
+            "TRACK": "relax.mp3"
+        }
+    }
+
     async def async_init(self):
         await self.bot.wait_until_ready()
         self.data_dict["GUILD"] = self.bot.guilds[0]
         self.data_dict["ERROR_CHANNEL"] = discord.utils.get(self.data_dict["GUILD"].text_channels, id=828638567236108308)
         self.data_dict["BOT_ID"] = self.bot.user.id
-        
-        if self.data_dict["BOT_ID"] == 755685507907846144:
-            target = 762082834235654164
-            self.data_dict["TRACK"] = "lofi.mp3"
-        
-        elif self.data_dict["BOT_ID"] == 756347306038657085:
-            target = 762150460651339806
-            self.data_dict["TRACK"] = "nature.mp3"
 
-        elif self.data_dict["BOT_ID"] == 762167641334218762:
-            target = 762171605244968990
-            self.data_dict["TRACK"] = "piano.mp3"
+        await self.get_bot_vc()
+        await self.connect_to_bot_vc()
+        
+        self.reconnect.start()
 
-        elif self.data_dict["BOT_ID"] == 820792275743014915:
-            target = 820793494419144845
-            self.data_dict["TRACK"] = "relax.mp3"
+    async def get_bot_songs_and_channel_ids(self) -> int:
+        """Checks the id of the bot, sets the track it has to play, and returns the id of the channel it is to connect to.
+        
+        Returns channel id"""
+        
+        try:
+            bot_dict = self.bot_dict[self.data_dict["BOT_ID"]]
+            self.data_dict["TRACK"] = bot_dict["TRACK"]
+        except KeyError:
+            print("This bot is not one of mine.")
+            raise SystemExit
             
-        else: print("That's not right"); raise SystemExit
+        print("Got the song and channel id.")
 
-        self.data_dict["VOICE_CHANNEL"] = discord.utils.get(self.data_dict["GUILD"].voice_channels, id=target)
+        return bot_dict["CHANNEL_ID"]
+
+    async def get_bot_vc(self):
+        """ Gets the vc of the bot. Exits if it does not exist"""
+        self.data_dict["VOICE_CHANNEL"] = discord.utils.get(self.data_dict["GUILD"].voice_channels, id=await self.get_bot_songs_and_channel_ids())
         if not self.data_dict["VOICE_CHANNEL"]:
-            print("Something went wrong.")
+            print("Could not find the voice channel for the bot to join.")
             await self.data_dict["ERROR_CHANNEL"].send("HEY BOSS, MY VOICE CHANNEL IS MISSIN'")
             raise SystemExit
-        
+
+        print("Got the bot's voice channel.")
+
+    async def connect_to_bot_vc(self):
         try:
             self.data_dict["VC_OBJECT"] = await self.data_dict["VOICE_CHANNEL"].connect()
         except:
-            print("Already connected")
-        self.reconnect.start()
+            print("Already connected, so disconnecting now")
+            await self.bot.voice_clients[0].disconnect()
+            await self.data_dict["VC_OBJECT"] = await self.data_dict["VOICE_CHANNEL"].connect()
+            print("Reconnected successfully")
 
-    @tasks.loop(minutes=1)
-    async def reconnect(self):
-        if not self.data_dict["VC_OBJECT"].is_connected():
-            try:
-                self.data_dict["VC_OBJECT"] = await self.data_dict["VOICE_CHANNEL"].connect()
-            except ClientException:
-                pass
-
-        if self.data_dict["VC_OBJECT"].channel != self.data_dict["VOICE_CHANNEL"]:
-            await self.data_dict["VC_OBJECT"].disconnect()
-            try:
-                self.data_dict["VC_OBJECT"] = await self.data_dict["VOICE_CHANNEL"].connect()
-            except ClientException:
-                pass
-
+    async def playtune(self):  
+        """Checks if the bot is currently playing a song. If not, then try to play it """      
         if not self.data_dict["VC_OBJECT"].is_playing():
             try:
-                await self.playtune()
+                self.data_dict["VC_OBJECT"].play(discord.FFmpegOpusAudio(self.data_dict["TRACK"]))
             except Exception as err:
                 print(f"An error occurred... Look at this {err}")
-                await self.data_dict["ERROR_CHANNEL"].send(f"An error occurred... Look at this:\n{err}")
+                await self.data_dict["ERROR_CHANNEL"].send(f"An error occurred... Look at this:\n{err}")            
+       
+    @tasks.loop(minutes=1)
+    async def reconnect(self):
+        if (not self.bot.voice_clients) or (self.data_dict["VC_OBJECT"].channel != self.data_dict["VOICE_CHANNEL"]):
+            await self.connect_to_bot_vc()
 
-    async def playtune(self):                    
-        self.data_dict["VC_OBJECT"].play(discord.FFmpegOpusAudio(self.data_dict["TRACK"]))
+        await self.playtune()
 
     @commands.Cog.listener()
     async def on_message(self, message):
